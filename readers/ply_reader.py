9ddc94d74374f8756e8cc4950c288bb5bcfead55
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2016-02-14 14:09:58 -0200

    Move PLY reader implementation to a new PlyReader class

--- readers/ply_reader.py
+++ readers/ply_reader.py
@@ -1,59 +1,74 @@
 from geometry.polyhedron import Polyhedron
 
 
-
-STATE_READING_HEADER = 0
-STATE_READING_BODY = 1
-
 def readPly(filename):
     '''
-    Reader for the .ply file format. This reader is really very dummy and I'm
-    only using it because it is easier to model 3d stuff in blender and then
-    read it instead of making polyhedrons by hand :P
+    Helper method to read ply files.
 
     :param unicode filename:
-    :returns Polyhedron:
     '''
-    points = []
-    faces = []
-    with open(filename, 'r') as file_contents:
-        state = STATE_READING_HEADER
-        for n, line in enumerate(file_contents):
-            if 'comment' in line:
-                # Ignore comment lines
-                continue
+    reader = PlyReader()
+    return reader.read(filename)
 
-            if state == STATE_READING_HEADER:
-                if n == 0:
-                    assert 'ply' in line, \
-                        "Expected ply in document's first line, but got " + unicode(line)
-                    continue
-                elif n == 1:
-                    assert 'format ascii 1.0' in line, \
-                        "This reader only supports format ascii 1.0"
-                    continue
 
-                if 'element' in line:
-                    if 'vertex' in line:
-                        number_of_vertexes = int(line.split()[-1])
-                    elif 'face' in line:
-                        number_of_faces = int(line.split()[-1])
-                    else:
-                        print "Unknown element " + unicode(line)
-                    continue
+class PlyReader(object):
 
-                if 'end_header' in line:
-                    state = STATE_READING_BODY
-                    continue
+    # Internal reader states
+    READING_HEADER = 0
+    READING_BODY = 1
 
-            elif state == STATE_READING_BODY:
-                if number_of_vertexes:
-                    x, y, z, _, _, _ = line.split()
-                    points.append([float(x), float(y), float(z)])
-                    number_of_vertexes -= 1
-                    continue
-                elif number_of_faces:
-                    faces.append([int(i) for i in line.split()[1:]])
-                    number_of_faces -= 1
-                    continue
-    return Polyhedron(points, faces)
+    def __init__(self):
+        self._state = self.READING_HEADER
+        self._state_to_method = {
+            self.READING_HEADER: self._read_header,
+            self.READING_BODY: self._read_body,
+        }
+        self._number_of_vertexes = 0
+        self._number_of_faces = 0
+        self._points = []
+        self._faces = []
+
+
+    def _read_body(self, n, line):
+        if self._number_of_vertexes:
+            x, y, z, _, _, _ = line.split()
+            self._points.append([float(x), float(y), float(z)])
+            self._number_of_vertexes -= 1
+            return
+
+        if self._number_of_faces:
+            self._faces.append([int(i) for i in line.split()[1:]])
+            self._number_of_faces -= 1
+            return
+
+
+    def _read_header(self, n, line):
+        if n == 0:
+            assert 'ply' in line, \
+                "Expected ply in document's first line, but got " + unicode(line)
+            return
+
+        if n == 1:
+            assert 'format ascii 1.0' in line, \
+                "This reader only supports format ascii 1.0"
+            return
+
+        if 'element' in line:
+            if 'vertex' in line:
+                self._number_of_vertexes = int(line.split()[-1])
+            elif 'face' in line:
+                self._number_of_faces = int(line.split()[-1])
+            else:
+                print "Unknown element " + unicode(line)
+            return
+
+        if 'end_header' in line:
+            self._state = self.READING_BODY
+            return
+
+
+    def read(self, filename):
+        with open(filename, 'r') as file_contents:
+            for n, line in enumerate(file_contents):
+                self._state_to_method[self._state](n, line)
+        return Polyhedron(self._points, self._faces)
