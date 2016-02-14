69cc8326e2783f54b17b65efe6b43eba5eecfdb2
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2016-02-14 13:17:11 -0200

    ply_reader test minor refactoring

--- readers/_tests/test_ply_reader.py
+++ readers/_tests/test_ply_reader.py
@@ -6,35 +6,9 @@ import numpy as np
 
 def testReadPlyCube():
     current_path = os.path.dirname(os.path.abspath(__file__))
+    expected_cube_points = np.loadtxt(current_path + "/test_ply_reader/expected_cube_points.txt")
     polyhedron = readPly(current_path + "/test_ply_reader/cube.ply")
+
     assert len(polyhedron.getFaces()) == 6
     assert len(polyhedron.getPoints()) == 24
-    assert np.allclose(
-        polyhedron.getPoints(),
-        np.array([
-            [ 1.0, 1.0, -1.0],
-            [ 1.0, -1.0, -1.0],
-            [-1.0, -1.0, -1.0],
-            [-1.0, 1.0, -1.0],
-            [ 1.0, 1.0, 1.0],
-            [-1.0, 1.0, 1.0],
-            [-1.0, -1.0, 1.0],
-            [ 1.0, -1.0, 1.0],
-            [ 1.0, 1.0, -1.0],
-            [ 1.0, 1.0, 1.0],
-            [ 1.0, -1.0, 1.0],
-            [ 1.0, -1.0, -1.0],
-            [ 1.0, -1.0, -1.0],
-            [ 1.0, -1.0, 1.0],
-            [-1.0, -1.0, 1.0],
-            [-1.0, -1.0, -1.0],
-            [-1.0, -1.0, -1.0],
-            [-1.0, -1.0, 1.0],
-            [-1.0, 1.0, 1.0],
-            [-1.0, 1.0, -1.0],
-            [ 1.0, 1.0, 1.0],
-            [1.0, 1.0, -1.0],
-            [-1.0, 1.0, -1.0],
-            [-1.0, 1.0, 1.0],
-        ])
-    )
+    assert np.allclose(polyhedron.getPoints(), expected_cube_points)
