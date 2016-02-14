36e37b853da2dcfeee8ca037c9b894459d82a273
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2016-02-14 16:47:55 -0200

    Add sphere ply read test

--- readers/_tests/test_ply_reader.py
+++ readers/_tests/test_ply_reader.py
@@ -1,14 +1,29 @@
 import os
+import pytest
 
 from readers.ply_reader import readPly
 import numpy as np
 
 
-def testReadPlyCube():
+def getTestFile(filename):
+    '''
+    :param unicode filename:
+    '''
     current_path = os.path.dirname(os.path.abspath(__file__))
-    expected_cube_points = np.loadtxt(current_path + "/test_ply_reader/expected_cube_points.txt")
-    polyhedron = readPly(current_path + "/test_ply_reader/cube.ply")
+    return current_path + "/test_ply_reader/" + filename
 
-    assert len(polyhedron.getFaces()) == 6
-    assert len(polyhedron.getPoints()) == 24
+
+@pytest.mark.parametrize(
+    ('ply_name', 'expected_number_of_faces', 'expected_number_of_points'),
+    [
+        ('cube', 6, 24),
+        ('sphere', 840, 422),
+    ],
+)
+def testReadPlyCube(ply_name, expected_number_of_faces, expected_number_of_points):
+    expected_cube_points = np.loadtxt(getTestFile("expected_" + ply_name + "_points.txt"))
+    polyhedron = readPly(getTestFile(ply_name + ".ply"))
+
+    assert len(polyhedron.getFaces()) == expected_number_of_faces
+    assert len(polyhedron.getPoints()) == expected_number_of_points
     assert np.allclose(polyhedron.getPoints(), expected_cube_points)
